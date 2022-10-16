d083afd1fd509d5a3ddc8cfa15ab6d00f79dcefd
Juho Rutila <juho.rutila@iki.fi>
2022-10-16 17:18:36 +0300

    working test

--- test/test.ts
+++ test/test.ts
@@ -1,10 +1,12 @@
 'use strict'
-import * from 'path'
+import 'mocha'
+import * as path from 'path'
 import generate from 'markdown-it-testgen'
+import when from '../src/'
 
 describe('default when', () => {
   const md = require('markdown-it')()
-              .use(require('../src/'))
+              .use(when)
 
   generate(path.join(__dirname, 'fixtures/default.txt'), md)
 })
