e8544a079b4d2b0ebd0be6360ef1841d99515fd8
Juho Rutila <juho.rutila@gmail.com>
2025-03-27 11:15:18 +0200

    test: add test for endRegex

---
+++ test/regex.spec.ts
@@ -0,0 +1,33 @@
+'use strict'
+import * as path from 'path'
+import generate from 'markdown-it-testgen'
+import when from '../src'
+import md from 'markdown-it'
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+describe('default when', () => {
+  const mdTest = md()
+              .use(when, {
+                whenBlockOpen: function (tokens, idx, _options, env, slf) {
+                  const token = tokens[idx]
+                  return `<p ${token.meta.phrase}="${token.meta.param}">\n`
+                },
+                whenBlockClose: function (tokens, idx, _options, env, slf) {
+                  return `</p>\n`
+                },
+                whenOpen: function (tokens, idx, _options, env, slf) {
+                  const token = tokens[idx]
+                  return `<span ${token.meta.phrase}="${token.meta.param}">`
+                },
+                whenClose: function (tokens, idx, _options, env, slf) {
+                  return `</span>`
+                },
+                startRegex: '--$0 ([a-z]*?)--',
+                endRegex: "--\\/$0--",
+              })
+
+  generate(path.join(__dirname, 'fixtures/special-tags.txt'), mdTest)
+})
